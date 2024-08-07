6369284bee8364c8fc79e919e1c5c25a7482718f
Brad Germain <bradgermain@Brads-MacBook-Pro.local>
2024-08-07 18:03:10 -0400

    Wezterm configuration files

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+config = {
+	automatically_reload_config = true,
+	enable_tab_bar = false,
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "RESIZE", -- disable the title bar but enable the resizable border
+	default_cursor_style = "BlinkingBar",
+	color_scheme = "Nord (Gogh)",
+	font = wezterm.font("JetBrains Mono", { weight = "Bold" }),
+	font_size = 12.5,
+
+}
+
+-- and finally, return the configuration to wezterm
+return config
